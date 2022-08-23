76a170cc8ea4f2d65e2babf97178f2b1ad91dae0
YOOJUNGIL <xcon9999@gmail.com>
2022-08-23 20:55:50 +0900

    initial commit

---
+++ src/main/java/com/myalley/eclipsenew/SpringEclipseNewApplication.java
@@ -0,0 +1,13 @@
+package com.myalley.eclipsenew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEclipseNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEclipseNewApplication.class, args);
+	}
+
+}
